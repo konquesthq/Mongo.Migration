2ac8e9792d185589dff8f205581e1c1dcaf7c0c6
Anth Richardson <anthrblahblah@gmail.com>
2024-12-11 17:13:45 +0000

    MongoMigrationService: use IServiceScopeFactory to get IMongoMigration

--- Mongo.Migration/Startup/DotNetCore/MongoMigrationService.cs
+++ Mongo.Migration/Startup/DotNetCore/MongoMigrationService.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Mongo.Migration.Startup.DotNetCore;
 
-public class MongoMigrationService(IMongoMigration mongoMigration, ILoggerFactory loggerFactory)
+public class MongoMigrationService(IServiceScopeFactory serviceScopeFactory, ILoggerFactory loggerFactory)
     : BackgroundService
 {
     private readonly ILogger<MongoMigrationService> _logger = loggerFactory.CreateLogger<MongoMigrationService>();
+    private readonly IMongoMigration _migration =
+        serviceScopeFactory.CreateScope().ServiceProvider.GetService<IMongoMigration>();
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
             _logger.LogInformation("Running migration. Please wait....");
-            mongoMigration.Run();
+
+            _migration.Run();
+
             _logger.LogInformation("Migration has been done");
         }
         catch (Exception ex)
         {
-            var exType = ex.GetType().ToString();
-            _logger.LogError(ex, "Error during migration: {ExType}", exType);
+            var errorType = ex.GetType().ToString();
+            _logger.LogError(ex, "An error occured during migration: {ErorType}", errorType);
         }
         
         return Task.CompletedTask;
